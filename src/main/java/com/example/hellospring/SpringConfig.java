d178a002fe1847f366f8f118354645f50fa121e4
YOONJI KIM <stritegdc@gmail.com>
2022-03-13 23:37:30 +0900

    [feat]: 순수 jdbc로 repository 구현해보기

--- src/main/java/com/example/hellospring/SpringConfig.java
+++ src/main/java/com/example/hellospring/SpringConfig.java
@@ -1,15 +1,23 @@
 package com.example.hellospring;
 
+import com.example.hellospring.repository.JdbcMemberRepository;
 import com.example.hellospring.repository.MemberRepository;
-import com.example.hellospring.repository.MemoryMemberRepository;
 import com.example.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.beans.BeanProperty;
+import javax.sql.DataSource;
 
 @Configuration
 public class SpringConfig {
+    private final DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
     @Bean
     public MemberService memberService(){
         return new MemberService(memberRepository());
@@ -17,7 +25,8 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository(){
-        return new MemoryMemberRepository();
+       // return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 
 }
