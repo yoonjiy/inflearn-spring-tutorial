a998e432b8bc76c7270c4f0d2338f2bd8538d81c
YOONJI KIM <stritegdc@gmail.com>
2022-03-14 00:18:11 +0900

    [feat]: JdbcTemplate을 이용한 멤버 리포지토리

--- src/main/java/com/example/hellospring/SpringConfig.java
+++ src/main/java/com/example/hellospring/SpringConfig.java
@@ -1,6 +1,7 @@
 package com.example.hellospring;
 
 import com.example.hellospring.repository.JdbcMemberRepository;
+import com.example.hellospring.repository.JdbcTemplateRepository;
 import com.example.hellospring.repository.MemberRepository;
 import com.example.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,7 +27,8 @@ public class SpringConfig {
     @Bean
     public MemberRepository memberRepository(){
        // return new MemoryMemberRepository();
-        return new JdbcMemberRepository(dataSource);
+       // return new JdbcMemberRepository(dataSource);
+        return new JdbcTemplateRepository(dataSource);
     }
 
 }
